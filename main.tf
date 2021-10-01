f37addd9264a78d789d1c5b29f5e0a9ee13b5284
Melvin C V <melvin.v@fsc.live>
2021-10-01 22:02:18 +0530

    added tf files

---
+++ main.tf
@@ -0,0 +1,25 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.27"
+    }
+  }
+
+  required_version = ">= 0.14.9"
+}
+
+provider "aws" {
+  profile = "cloudpixels"
+  region  = "ap-southeast-1"
+}
+
+resource "aws_instance" "appserver" {
+  ami             = "ami-00bd7cc070262f111"
+  instance_type   = "t2.micro"
+  key_name = "tutorial-key"
+
+  tags = {
+    Name = var.instance_name
+  }
+}
