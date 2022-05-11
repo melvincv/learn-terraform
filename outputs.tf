fbf6af5bf32b92dd5e20b4c6508e24a329a33570
Melvin C Varghese <melrockz@gmail.com>
2022-05-11 17:01:41 +0530

    clear all files after creating a branch (old-sl-course)

--- outputs.tf
+++
@@ -1,9 +0,0 @@
-output "instance_id" {
-  description = "ID of the EC2 instance"
-  value       = aws_instance.appserver.id
-}
-
-output "instance_public_ip" {
-  description = "Public IP address of the EC2 instance"
-  value       = aws_instance.appserver.public_ip
-}
